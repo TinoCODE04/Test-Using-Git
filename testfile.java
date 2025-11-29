017d1f7a35e0147e7d6c049bf8e8824a1b443467
TinoCODE04 <tinoekalin@gmail.com>
2025-11-29 16:02:30 +0800

    add testfile.java

---
+++ testfile.java
@@ -0,0 +1,8 @@
+public class testfile {
+    public static void main(String args[]){
+        int a = 1;
+        int b = 2;
+        int result = a + b;
+        System.out.println("result = " + result);
+    }
+}
