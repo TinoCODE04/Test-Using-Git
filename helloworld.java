3751d7f57003cdcda4de05a4274e088906de43c6
TinoCODE04 <tinoekalin@gmail.com>
2025-11-29 17:20:56 +0800

    add !! to helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 public class helloworld {
     public static void main(String[] args) {
     System.err.println("Hello");
-    System.out.println("i am tino");
+    System.out.println("i am tino!!");
 }
 }
